1022fba0aa7c908db83bf4017e26f86697b6b4ca
songjisoo97 <jisoosong97@gmail.com>
2022-11-29 17:30:09 +0900

    [update] PORT 수정"

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require('express');
-require('dotenv').config();
 
-const PORT = process.env.PORT;
+const PORT = 3000;
 
 const postRouter = require('./routes/post.js');
 const commentRouter = require('./routes/comment.js');
